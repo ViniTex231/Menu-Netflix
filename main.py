665162021308a3c48751d206259911805db7388b
ViniTex231 <126170717+ViniTex231@users.noreply.github.com>
2023-03-14 11:25:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from netflix import Cliente
 from menu import menu, novo_usuario
 
 menu()
-print(novo_usuario)
+print(f'Escolha um filme {novo_usuario[0]},'
+      f'lembrando que você é {novo_usuario[2]}')
